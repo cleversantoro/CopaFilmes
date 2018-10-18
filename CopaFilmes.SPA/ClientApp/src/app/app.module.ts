e17cd61964436679f2191d0a8284070bbe77b2e2
Clever Santoro Lopes <cleversantoro@gmail.com>
2018-10-17 23:28:20 -0300

    CopaFilmes SPA

---
+++ CopaFilmes.SPA/ClientApp/src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, PreloadAllModules } from '@angular/router';
+
+import { ROUTES } from './app.routes';
+
+import { AppComponent } from './app.component';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
+import { HomeComponent } from './home/home.component';
+import { FilmesComponent } from './filmes/filmes.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavMenuComponent,
+    HomeComponent,
+    FilmesComponent
+  ],
+  imports: [
+    BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
+    HttpClientModule,
+    FormsModule,
+    RouterModule.forRoot(ROUTES, { preloadingStrategy: PreloadAllModules })
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ CopaFilmes.SPA/ClientApp/src/app/app.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router'
+
+import { HomeComponent } from './home/home.component'
+import { FilmesComponent } from './filmes/filmes.component'
+
+export const ROUTES: Routes = [
+  { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: 'filmes', component: FilmesComponent },
+]
